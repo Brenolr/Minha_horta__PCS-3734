c3abd1a4020e91e093428e941b8839839cc36636
APF2000 <arthurpfonseca@gmail.com>
2021-07-30 15:58:25 -0300

    Procurando POST requests

--- Server/mysite/polls/views.py
+++ Server/mysite/polls/views.py
@@ -2,4 +2,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	if request.method == 'GET':
+		return HttpResponse("Batatinha quando nasce")
+	elif request.method == 'POST':
+		return HttpResponse("Recebido: ", request.POST)
+
+	return HttpResponse("Hello, world. You're at the polls index.")
