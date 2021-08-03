daaefd37b11103b6aba698cd3ea48080f3b929c4
APF2000 <arthurpfonseca@gmail.com>
2021-08-03 10:21:30 -0300

    Hackeando o Django: POST sem cookies

--- Server/mysite/polls/views.py
+++ Server/mysite/polls/views.py
@@ -4,6 +4,13 @@ def index(request):
 	if request.method == 'GET':
 		return HttpResponse("Batatinha quando nasce")
 	elif request.method == 'POST':
-		return HttpResponse("Recebido: ", request.POST)
+		response = HttpResponse("Recebido: ", request.POST)
+		try:
+			value = request.COOKIES['cookie_name']
+		except KeyError:
+		# Cookie is not set
+			response.set_cookie('cookie_name', 'cookie_value')
+		
+		return response
 
 	return HttpResponse("Hello, world. You're at the polls index.")
