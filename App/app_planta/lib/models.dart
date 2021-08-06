87408a5bd1e50a3db48cd3c26bd02b9ac1e14e41
Brenolr <breno.loscher@mobile2you.com.br>
2021-08-06 13:38:50 -0300

    commit para merge

--- App/app_planta/lib/models.dart
+++ App/app_planta/lib/models.dart
@@ -5,3 +5,26 @@ class OrdinalSales {
 
   OrdinalSales(this.year, this.sales);
 }
+
+class Data {
+  final List<double> temp;
+  final List<double> light;
+  final List<double> hum_air;
+  final List<int> hum_soil;
+
+  Data({
+    this.temp,
+    this.light,
+    this.hum_air,
+    this.hum_soil
+  });
+
+  factory Data.fromJson(Map<String, dynamic> json) {
+    return Data(
+      temp: json['temp'],
+      light: json['light'],
+      hum_air: json['hum_air'],
+      hum_soil: json['hum_soil'],
+    );
+  }
+}
