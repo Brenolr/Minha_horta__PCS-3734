6893c74ece7329c8b91787cd10d780fee7ff4da5
APF2000 <arthurpfonseca@gmail.com>
2021-08-06 13:38:28 -0300

    Melhorando resposta do servidor: JSON

--- Server/mysite/polls/urls.py
+++ Server/mysite/polls/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', 							views.index,	name='index'),
-    path('measure/<slug:measure_name>', views.measure, 	name='a')
+    path('measure/<slug:measure_name>', views.measure, 	name='a'),
+    path('threshold/<slug:measure_name>', views.threshold, 	name='b'),
 ]
