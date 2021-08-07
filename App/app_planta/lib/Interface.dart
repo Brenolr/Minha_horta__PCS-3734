ad7eba5bd126133b45ea1984ff14e7f0971c6d5b
Brenolr <breno.loscher@mobile2you.com.br>
2021-08-07 19:38:02 -0300

    bora tá quase

--- App/app_planta/lib/Interface.dart
+++ App/app_planta/lib/Interface.dart
@@ -2,16 +2,16 @@
   import 'package:charts_flutter/flutter.dart' as charts;
 
 /// Create one series with sample hard coded data.
-  List<charts.Series<OrdinalSales, String>> getHumidity() {
+  List<charts.Series<OrdinalSales, int>> getHumidity() {
     final data = [
-      new OrdinalSales('2014', 5),
-      new OrdinalSales('2015', 25),
-      new OrdinalSales('2016', 100),
-      new OrdinalSales('2017', 75),
+      new OrdinalSales(2014, 5),
+      new OrdinalSales(2015, 25),
+      new OrdinalSales(2016, 100),
+      new OrdinalSales(2017, 75),
     ];
 
     return [
-      new charts.Series<OrdinalSales, String>(
+      new charts.Series<OrdinalSales, int>(
         id: 'Sales',
         colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
         domainFn: (OrdinalSales sales, _) => sales.year,
