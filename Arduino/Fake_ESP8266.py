1bb1ca67631c2ef22609264a0aaadf0bd39723a5
Brenolr <breno.loscher@mobile2you.com.br>
2021-09-01 14:21:05 -0300

    teste

--- Arduino/Fake_ESP8266.py
+++ Arduino/Fake_ESP8266.py
@@ -3,17 +3,24 @@ import json
 import re  # regx
 import requests 
 
+
 def main():
-    
+    motor_on = 1
     ser = serial.Serial('COM9',115200)
+
     while(True):
         char = clean_line(ser.readline())
         if(char== "|"):
-            umidade_solo = clean_line(ser.readline())
-            umidade_ar = clean_line(ser.readline())
-            temp = clean_line(ser.readline())
-            luz = clean_line(ser.readline())
+            umidade_solo = float(clean_line(ser.readline()))
+            umidade_ar = float(clean_line(ser.readline()))
+            temp = float(clean_line(ser.readline()))
+            luz = float(clean_line(ser.readline()))
+            ser.write(bytes("teste", 'utf-8'))
             send_data(json.dumps({"umid_ar": float(umidade_ar), "luz":float(luz), "temp": float(temp), "umid_solo": float(umidade_solo)}))
+            lim_umidade = get_humidity_limits()
+            motor_on = engine_control(lim_umidade,umidade_solo,motor_on)
+            ser.write(bytes(str(motor_on), 'utf-8'))
+        print(ser.readline())
     
     # send_data(json.dumps({"umid_ar": 2.1, "luz": 50.7, "temp": 23.6, "umid_solo": 50.0}))  
     #send_data({"umid_ar": 2.1, "luz": 50.0, "temp": 23.6, "umid_solo": 5})
@@ -24,6 +31,26 @@ def send_data(data):
         print("Deu bom")
     except:
         print("Conection error")
+
+def get_humidity_limits():
+    try:
+        data = requests.get(url = "http://localhost:8000/polls/threshold/geral")
+        print("Deu bom tresh")
+        data = json.loads(data.text)
+        return(data["umid_solo"][len(data["umid_solo"])-1])
+    except:
+        print("Conection error")
+    
+def engine_control(lim_umidade,umidade,motor_on):
+    print(lim_umidade)
+    if((lim_umidade["min_value"])>umidade):
+        print("Deveria ligar")
+        return(1)
+    if(lim_umidade["max_value"]<=umidade):
+        print("Deveria Desligar")
+        return(0)
+    print("Deveria Manter")
+    return(motor_on)
 
 def clean_line(bruto):
     bruto = str(bruto)
--- Arduino/base/base.ino
+++ Arduino/base/base.ino
@@ -7,12 +7,15 @@
 #define DHTPIN 7 //PINO DIGITAL UTILIZADO PELO DHT22
 #define DHTTYPE DHT22 //DEFINE O MODELO DO SENSOR (DHT22 / AM2302)
 
+//BH1750 lightMeter(0x23);
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(Motor, OUTPUT);
   Serial.begin(115200);
+//  lightMeter.begin(BH1750::ONE_TIME_LOW_RES_MODE);
   Wire.begin(); //INICIALIZA O I2C BUS
   dht.begin(); 
 //  lightMeter.begin();
@@ -23,20 +26,35 @@ void setup()
   int humidade;
   float humidade_ar;
   float temp;
+  int controle;
+  float luz;
+
   humidade = hidrometro();
   humidade_ar = DHT22_humidade();
   temp = DHT22_tempe();
+  luz = BH1750_luz();
   engine_controler(humidade, 100, 10);
   Serial.print("|\n");
   Serial.print(humidade);
   Serial.print("\n");
-  Serial.print(humidade_ar);
+  Serial.print(humidade_ar);//umidade ar
   Serial.print("\n");
-  Serial.print(temp);
+  Serial.print(temp);//temp
   Serial.print("\n");
-  Serial.print("45.0");
+  Serial.print(luz);//luz
   Serial.print("\n");
-  delay(5000);
+  controle = Serial.readString().toInt();
+  if(controle == 1){
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.print("Motor ligado\n");
+  }
+   else{
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.print("Motor desligado\n");
+   }
+   
+  delay(10000);
+  
 }
 
 void engine_controler(int humidade, int limite_superior, int limite_inferior)
@@ -49,6 +67,15 @@ void engine_controler(int humidade, int limite_superior, int limite_inferior)
 
 int hidrometro(){
   int valor_analogico = analogRead(pino_hidrometro);
+//   Serial.print(valor_analogico);
+//   Serial.print("\n");
+  valor_analogico = 1023 - valor_analogico;
+//  Serial.print(valor_analogico);
+//   Serial.print("\n");
+  valor_analogico = valor_analogico*100;
+  valor_analogico = valor_analogico/1024;
+//  Serial.print(valor_analogico);
+//   Serial.print("\n");
   return(valor_analogico);
 }
 
@@ -62,7 +89,11 @@ int DHT22_tempe(){
   return(temp);
 }
 
-//float BH1750_luz(){
+float BH1750_luz(){
 //  float lux = lightMeter.readLightLevel();
-//  return(lux);
-//}
+  double lux= 50.0;
+  if(lux <0){
+    return 0;
+  }
+  return(lux);
+}
